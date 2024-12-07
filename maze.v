ef63892b8c94201c3b758937c0ce689421b95997
lemonade <138853473+1608104089@users.noreply.github.com>
2024-12-07 15:45:12 +0800

    Add files via upload

---
+++ maze.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2024/12/07 15:28:23
+// Design Name: 
+// Module Name: maze
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module maze(
+    input clk,
+    input rst
+    );
+endmodule
